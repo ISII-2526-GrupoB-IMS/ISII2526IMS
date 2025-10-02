1e7e9c33392eff4ee09fd2ed2695c300ef519fd9
MIGUEL ÁNGEL FELIPE LÓPEZ <MiguelAngel.Felipe@alu.uclm.es>
2025-10-02 18:42:26 +0200

    Creacion clase Dispositivo

---
+++ src/AppForSEII2526.API/Models/Dispositivo.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(NombreDispositivo), IsUnique = true)] //Nombre es UNIQUE
+    public class Dispositivo
+    {
+        public Dispositivo()
+        {
+        }
+
+        public Dispositivo(Modelo modelo, string marca, string color, string nombreDispositivo, double precioParaCompra, double precioParaAlquiler, int cantidadParaCompra, int cantidadParaAlquiler, IList<ItemCompra> itemsCompra, IList<ItemAlquiler> itemsAlquiler, IList<ItemReseña> itemsReseña, int año)
+        {
+            Modelo = modelo;
+            Marca = marca; 
+            Color = color;
+            NombreDispositivo = nombreDispositivo;  
+            PrecioParaCompra = precioParaCompra;
+            PrecioParaAlquiler = precioParaAlquiler;
+            CantidadParaCompra = cantidadParaCompra;
+            CantidadParaAlquiler = cantidadParaAlquiler;
+            ItemsCompra = itemsCompra;  
+            ItemsAlquiler = itemsAlquiler;
+            ItemsReseña = itemsReseña;
+            Año = año;
+        }
+
+        //ID Dispositivo
+        [Key]
+        public int Id { get; set; }
+
+        //MODELO
+        [Required]
+        public Modelo Modelo { get; set; }
+
+        //MARCA
+        [Required]
+        [StringLength(50, ErrorMessage = "La marca no puede ser mayor de 50 caracteres")]
+        public string Marca { get; set; }
+
+        //COLOR
+        [Required]
+        [StringLength(20, ErrorMessage = "El color no puede ser mayor de 20 caracteres")]
+        public string Color { get; set; }
+
+        //NOMBRE DISPOSITIVO
+        [Required]
+        [StringLength(50, ErrorMessage = "El nombre del dispositivo no puede ser mayor de 50 caracteres")]
+        public string NombreDispositivo { get; set; }
+
+        //PRECIO COMPRA
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, float.MaxValue, ErrorMessage = "El precio mínimo es de 0,5 ")]
+        [Display(Name = "Precio para compra")]
+        [Precision(10, 2)]
+        public double PrecioParaCompra { get; set; }
+
+        //PRECIO ALQUILER
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, float.MaxValue, ErrorMessage = "El precio mínimo es de 0,5 ")]
+        [Display(Name = "Precio de Alquiler")]
+        public double PrecioParaAlquiler { get; set; }
+
+        //CANTIDAD PARA COMPRA
+        [Required]
+        [Display(Name = "Cantidad para comprar")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad mínima para comprar es 1")]
+        public int CantidadParaCompra { get; set; }
+
+        //CANTIDAD PARA ALQUILER
+        [Required]
+        [Display(Name = "Cantidad para alquilar")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad mínima para alquilar es 1")]
+        public int CantidadParaCompra { get; set; }
+
+
+        //AÑO
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1998, 2025, ErrorMessage = "El año mínimo es 1998 y el máximo 2025")]
+        [Display(Name = "Año")]
+        public double Año { get; set; }
+
+
+        public IList<ItemCompra> ItemsCompra { get; set; }
+        public IList<ItemAlquiler> ItemsAlquiler { get; set; }
+        public IList<ItemReseña> ItemsReseña { get; set; }
+
+
+
+
+
+    }
+
+}
+
+
