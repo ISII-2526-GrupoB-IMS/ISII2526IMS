9e4020116a16cc47b9019c75ec65daebf4940a6a
MiguelAngel2323 <127391360+MiguelAngel2323@users.noreply.github.com>
2025-10-07 10:34:27 +0200

    Task22 añadir clase Modelo a la carpeta Models (#38)
    * Creacion de la clase Modelo
    
    * Corrección clase modelo

---
+++ src/AppForSEII2526.API/Models/Modelo.cs
@@ -0,0 +1,29 @@
+﻿using NuGet.DependencyResolver;
+
+namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(NombreModelo), IsUnique = true)] //Nombre es UNIQUE
+    public class Modelo
+    {
+        public Modelo()
+        {
+        }
+
+        public Modelo(string nombreModelo)
+        {
+            NombreModelo = nombreModelo;
+        }
+
+        //ID Modleo
+        [Key] //Clave Primaria
+        public int Id { get; set; }
+
+        //NOMBRE MODELO
+        [Required]
+        [StringLength(50, ErrorMessage = "El nombre del modelo no puede ser mayor de 50 caracteres")]
+        public string NombreModelo { get; set; }
+
+
+        public IList<Dispositivo> Dispositivos { get; set; } = new List<Dispositivo>();
+    }
+}
