0f0030199e5a841fcea91c816164bea27b094881
MiguelAngel2323 <127391360+MiguelAngel2323@users.noreply.github.com>
2025-10-07 10:39:01 +0200

    Task34 modificación clase ApplicationDbContext (#39)
    Modificación de clase ApplicationDbContext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using AppForSEII2526.API.Models;
+using NuGet.DependencyResolver;
 
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Modelo> Modelo { get; set; }
+    public DbSet<Dispositivo> Dispositivo { get; set; }
+    public DbSet<Compra> Compra { get; set; }
+    public DbSet<ItemCompra> ItemCompra { get; set; }
 }
