4d81455b47f46670d126faa44b14ff34a7cfcf0d
Samuel150305 <Samuel.gomez3@alu.uclm.es>
2025-10-08 16:52:02 +0200

    Task 64 Modificación clase Dispositivo (#69)
    Modificación clase Dispositivo

--- src/AppForSEII2526.API/Models/Dispositivo.cs
+++ src/AppForSEII2526.API/Models/Dispositivo.cs
@@ -50,6 +50,7 @@ namespace AppForSEII2526.API.Models
         public IList<ItemCompra> ItemsCompra { get; set; }
 
         public IList<AlquilarDispositivo> DispositivosAlquilados { get; set; }
+        public IList<ItemReseña> ItemsReseña { get; set; }
 
     }
 
