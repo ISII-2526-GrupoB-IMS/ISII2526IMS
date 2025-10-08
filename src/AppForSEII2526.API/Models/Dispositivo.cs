74fc1cc479ef74374f2f5193cd405cd99d5f6439
ismaeljc68 <ismael.jimenez2@alu.uclm.es>
2025-10-08 13:01:19 +0200

    Añadido atributo dispositivos alquilados (#62)

--- src/AppForSEII2526.API/Models/Dispositivo.cs
+++ src/AppForSEII2526.API/Models/Dispositivo.cs
@@ -49,6 +49,7 @@ namespace AppForSEII2526.API.Models
 
         public IList<ItemCompra> ItemsCompra { get; set; }
 
+        public IList<AlquilarDispositivo> DispositivosAlquilados { get; set; }
 
     }
 
