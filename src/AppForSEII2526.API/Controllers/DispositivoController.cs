65b31485dc4efb5ee6754fc3a4c17d8bc6e821fb
MiguelAngel2323 <127391360+MiguelAngel2323@users.noreply.github.com>
2025-10-24 12:12:33 +0200

    Creación clases DeviceController y DispositivoParaComprarDTO (#93)
    * Creacion controller
    
    * Cracio del controller de dispositivo, el DTO para comprar y la inicializacion de la base de datos

---
+++ src/AppForSEII2526.API/Controllers/DispositivoController.cs
@@ -0,0 +1,54 @@
+﻿using AppForSEII2526.API.DTOs.DispositivoDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DispositivoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DispositivoController> _logger;
+
+        public DispositivoController(ApplicationDbContext context,
+            ILogger<DispositivoController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<DispositivoParaComprarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetDispositivosParaComprar()
+        {
+            var dispositivos = await _context.Dispositivo
+                .Select(d => new DispositivoParaComprarDTO(
+                    d.Id,
+                    d.NombreDispositivo,
+                    d.Marca,
+                    d.Modelo,
+                    d.Color,
+                    d.PrecioParaCompra
+                ))
+                .ToListAsync();
+            return Ok(dispositivos);
+        }
+    }
+}
